b66d5de4503d1d9aac7763aff15976c9bfbb942f
TheYingineer <ying.yan.huang87@gmail.com>
2022-08-28 15:51:16 -0500

    ready to submit react router portion

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import cars from '../cars.json'
-// Import {useParams} from "react-router-dom" here //
+import { useParams } from "react-router-dom"
+import { Container, Paper, Chip } from "@mui/material";
+
 
 // import MUI components here //
 // Container, Paper, Chip //
 
 const Car = (props) => {
+    const { id } = useParams();
+    const foundCar = cars.find((car) => {
+        return car.id == id;
+    })
+    console.log(foundCar);
     return (
-        <h1>A specific car</h1>
+        <Container maxWidth="sm" className="car-container">
+        <Paper className="car-paper">
+          <h2>{foundCar.Name}</h2>
+          {Object.keys(foundCar).map((key, idx) => {
+            return <Chip key={idx} label={`${key}: ${foundCar[key]}`}></Chip>;
+          })}
+        </Paper>
+      </Container>
     )
+
 }
 
 export default Car
